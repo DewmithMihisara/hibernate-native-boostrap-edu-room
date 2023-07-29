9f815899de30dd3b71bbc09ec7c8cc8552111373
dewmithmihisara <mihisaralokuhewage@>
2023-07-29 13:17:56 +0530

    apply singleton into SessionFactoryConfig

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,4 +1,9 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
 public class SessionFactoryConfig {
+    private static SessionFactoryConfig factoryConfig;
+    private SessionFactoryConfig(){}
+    public static SessionFactoryConfig getInstance(){
+
+    }
 }
