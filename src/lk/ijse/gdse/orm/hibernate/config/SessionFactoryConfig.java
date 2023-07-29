0578e821e9b5b3ca22f02b256256c20e87b9f619
dewmithmihisara <mihisaralokuhewage@>
2023-07-29 13:39:45 +0530

    add hibernate jar files

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -2,8 +2,10 @@ package lk.ijse.gdse.orm.hibernate.config;
 
 public class SessionFactoryConfig {
     private static SessionFactoryConfig factoryConfig;
-    private SessionFactoryConfig(){}
-    public static SessionFactoryConfig getInstance(){
+    private SessionFactoryConfig(){
 
+    }
+    public static SessionFactoryConfig getInstance(){
+        return (factoryConfig == null)?factoryConfig=new SessionFactoryConfig():factoryConfig;
     }
 }
