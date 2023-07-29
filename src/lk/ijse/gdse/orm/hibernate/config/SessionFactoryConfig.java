cb76ed87abd4fa651145e7859d8aa03c5284e547
dewmithmihisara <mihisaralokuhewage@>
2023-07-29 13:57:33 +0530

    implement service registry

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,11 +1,16 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
+import org.hibernate.Session;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class SessionFactoryConfig {
     private static SessionFactoryConfig factoryConfig;
-    private SessionFactoryConfig(){
-
-    }
+    private SessionFactoryConfig(){}
     public static SessionFactoryConfig getInstance(){
         return (factoryConfig == null)?factoryConfig=new SessionFactoryConfig():factoryConfig;
+    }
+    public Session getSession(){
+        StandardServiceRegistry serviceRegistry=new StandardServiceRegistryBuilder().configure().build();
     }
 }
