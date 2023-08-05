3764d4b59b6cd2307dfc18100fb34469ba4f52ca
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-08-05 14:03:10 +0530

    impliment get Customer

--- src/lk/ijse/gdse/orm/hibernate/ripository/CustomrtRipository.java
+++ src/lk/ijse/gdse/orm/hibernate/ripository/CustomrtRipository.java
@@ -1,5 +1,15 @@
 package lk.ijse.gdse.orm.hibernate.ripository;
 
-public class CustomrtRipository {
+import lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig;
+import lk.ijse.gdse.orm.hibernate.entity.Customer;
+import org.hibernate.Session;
 
+public class CustomrtRipository {
+    private final Session session;
+    public CustomrtRipository(){
+        session= SessionFactoryConfig.getInstance().getSession();
+    }
+    public Customer getCustomer(int id){
+        return session.get(Customer.class,1);
+    }
 }
